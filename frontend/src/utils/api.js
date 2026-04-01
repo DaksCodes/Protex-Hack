443a3da65a6b938f8b94bb70c24b556f95007eda
Sneha Yadav <you@example.com>
2026-04-01 10:33:13 +0530

    fix: update API base URL for deployed backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,20 @@
-const BASE_URL = "http://127.0.0.1:8000";
+// const BASE_URL = "http://127.0.0.1:8000";
+
+// export const fetchSummary = async () => {
+//   const res = await fetch("http://127.0.0.1:8000/api/summary");
+
+//   if (!res.ok) {
+//     throw new Error("API not working");
+//   }
+
+//   return res.json();
+// };
+
+
+const BASE_URL = "https://protex-ml-backend.onrender.com";
 
 export const fetchSummary = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/summary");
+  const res = await fetch(`${BASE_URL}/api/summary`);
 
   if (!res.ok) {
     throw new Error("API not working");
@@ -10,7 +23,13 @@ export const fetchSummary = async () => {
   return res.json();
 };
 
+// export const fetchExpenses = async () => {
+//   const res = await fetch("http://127.0.0.1:8000/api/expenses");
+//   return res.json();
+// }
+
+
 export const fetchExpenses = async () => {
-  const res = await fetch("http://127.0.0.1:8000/api/expenses");
+  const res = await fetch(`${BASE_URL}/api/expenses`);
   return res.json();
-}
+};
